7b5dbb939d2841085177ded7d20a0b094e960c0f
Teanna Pearson <45774278+TeannaP18@users.noreply.github.com>
2022-04-18 12:54:08 -0400

    finished AdminHomeActivity class and added recycler view to activity layout file

--- app/src/main/java/cosc490/morgan/virtualvaccinationcard/AdminHomeActivity.java
+++ app/src/main/java/cosc490/morgan/virtualvaccinationcard/AdminHomeActivity.java
@@ -1,13 +1,37 @@
 package cosc490.morgan.virtualvaccinationcard;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class AdminHomeActivity extends AppCompatActivity {
+
+    private ArrayList<VaccinationModal> vaccinationModalArrayList;
+    private DBHandler dbHandler;
+    private VaccinationRVAdapter vaccinationRVAdapter;
+    private RecyclerView vaccinationsRV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+
+        vaccinationModalArrayList = new ArrayList<>();
+        dbHandler = new DBHandler(AdminHomeActivity.this);
+
+        vaccinationModalArrayList = dbHandler.readVaccinations();
+
+        vaccinationRVAdapter = new VaccinationRVAdapter(vaccinationModalArrayList, AdminHomeActivity.this);
+        vaccinationsRV.findViewById(R.id.VaccinationsRV);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(AdminHomeActivity.this, RecyclerView.VERTICAL, false);
+        vaccinationsRV.setLayoutManager(linearLayoutManager);
+
+        vaccinationsRV.setAdapter(vaccinationRVAdapter);
     }
 }

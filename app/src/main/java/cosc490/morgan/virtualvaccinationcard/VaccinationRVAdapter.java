3c06678ad084a0ea78d95dc43e38ee5c321e8a55
Teanna Pearson <45774278+TeannaP18@users.noreply.github.com>
2022-04-18 12:42:45 -0400

    finished VAccinationRVAdapter class

--- app/src/main/java/cosc490/morgan/virtualvaccinationcard/VaccinationRVAdapter.java
+++ app/src/main/java/cosc490/morgan/virtualvaccinationcard/VaccinationRVAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,16 +33,34 @@ public class VaccinationRVAdapter extends RecyclerView.Adapter<VaccinationRVAdap
     @Override
     public void onBindViewHolder(@NonNull VaccinationRVAdapter.ViewHolder holder, int position) {
         VaccinationModal modal = vaccinationModalArrayList.get(position);
+        holder.dose1DateTV.setText(modal.getDose1_date());
+        holder.dose1NumTV.setText(modal.getDose1_num());
+        holder.dose2DateTV.setText(modal.getDose2_date());
+        holder.dose2NumTV.setText(modal.getDose2_num());
+        holder.boosterDateTV.setText(modal.getBooster_date());
+        holder.boosterNumTV.setText(modal.getBooster_num());
+        holder.cardPhotoTV.setText(modal.getCard_photo());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return vaccinationModalArrayList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View view) {
-            super();
+
+        private TextView dose1DateTV, dose1NumTV, dose2DateTV, dose2NumTV, boosterDateTV, boosterNumTV, cardPhotoTV;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            dose1DateTV = itemView.findViewById(R.id.tvDose1Date);
+            dose1NumTV = itemView.findViewById(R.id.tvDose1Num);
+            dose2DateTV = itemView.findViewById(R.id.tvDose2Date);
+            dose2NumTV = itemView.findViewById(R.id.tvDose2Num);
+            boosterDateTV = itemView.findViewById(R.id.tvBoosterDate);
+            boosterNumTV = itemView.findViewById(R.id.tvBoosterNum);
+            cardPhotoTV = itemView.findViewById(R.id.imageVaccination);
         }
     }
 }

0bc6625629d9fc25e0f3764d5df0d6fc22e51e4f
Teanna Pearson <45774278+TeannaP18@users.noreply.github.com>
2022-04-30 23:55:14 -0400

    added backgrounds to pages

--- app/src/main/java/cosc490/morgan/virtualvaccinationcard/UserHomeActivity.java
+++ app/src/main/java/cosc490/morgan/virtualvaccinationcard/UserHomeActivity.java
@@ -1,21 +1,15 @@
 package cosc490.morgan.virtualvaccinationcard;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.ByteArrayOutputStream;
 
 @SuppressWarnings("ALL")
 public class UserHomeActivity extends AppCompatActivity {
